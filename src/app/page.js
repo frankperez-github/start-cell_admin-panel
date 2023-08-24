299a8c113e9e930a355cb3c7c864efb753def681
frankperez-github <fp848584@gmail.com>
2023-08-24 13:22:27 -0400

    connecting everything with firebase

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,10 @@
+'use client'
+import Image from 'next/image'
+import styles from './page.module.css'
+import Header from './components/Header'
+import {v4 as uuidv4} from 'uuid'
+import { useEffect, useState } from 'react'
+import useSiteContext from '@/Hooks/useSiteContext'
 
 export default function Home() {
 
