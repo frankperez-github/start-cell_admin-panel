d7e50134c70d48f0d0b9908b68036498ee5e208f
frankperez-github <fp848584@gmail.com>
2023-08-24 13:55:04 -0400

    connecting everything with firebase

--- src/Hooks/useSiteContext.jsx
+++ src/Hooks/useSiteContext.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import {db} from "@/app/firebase.config.js"
 
 export default function useSiteContext()
 {
--- src/app/firebase.config.js
+++ src/app/firebase.config.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// import {getStorage} from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +21,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore()
-// export const storage = getStorage(app)
