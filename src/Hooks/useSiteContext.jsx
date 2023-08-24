b2b2d5af68ab26ecf06cc41fb3f69a4357eb37f7
frankperez-github <fp848584@gmail.com>
2023-08-24 13:42:27 -0400

    connecting everything with firebase

--- src/Hooks/useSiteContext.jsx
+++ src/Hooks/useSiteContext.jsx
@@ -4,41 +4,41 @@ import {getDocs, collection } from "firebase/firestore"
 
 export default function useSiteContext()
 {
-    const [products, setProducts] = useState([])
-    const [clients, setClients] = useState([])
-    const [locations, setLocations] = useState([])
+    // const [products, setProducts] = useState([])
+    // const [clients, setClients] = useState([])
+    // const [locations, setLocations] = useState([])
 
-    const fetchInfo = async()=>{
-        const productsRef = collection(db, 'products')
-        const clientsRef = collection(db, 'clients')
-        const locationsRef = collection(db, 'locations')
-        const productsSnapshots = await getDocs(productsRef)
-        const clientsSnapshots = await getDocs(clientsRef)
-        const locationsSnapshots = await getDocs(locationsRef)
+    // const fetchInfo = async()=>{
+    //     const productsRef = collection(db, 'products')
+    //     const clientsRef = collection(db, 'clients')
+    //     const locationsRef = collection(db, 'locations')
+    //     const productsSnapshots = await getDocs(productsRef)
+    //     const clientsSnapshots = await getDocs(clientsRef)
+    //     const locationsSnapshots = await getDocs(locationsRef)
     
-        const productsDocs = productsSnapshots.docs.map((doc)=>{
-            const data = doc.data()
-            data.id = doc.id
-            return data
-        })
-        const clientsDocs = clientsSnapshots.docs.map((doc)=>{
-            const data = doc.data()
-            data.id = doc.id
-            return data
-        })
-        const locationsDocs = locationsSnapshots.docs.map((doc)=>{
-            const data = doc.data()
-            data.id = doc.id
-            return data
-        })
-        setProducts(productsDocs)
-        setClients(clientsDocs)
-        setLocations(locationsDocs)
-    }
+    //     const productsDocs = productsSnapshots.docs.map((doc)=>{
+    //         const data = doc.data()
+    //         data.id = doc.id
+    //         return data
+    //     })
+    //     const clientsDocs = clientsSnapshots.docs.map((doc)=>{
+    //         const data = doc.data()
+    //         data.id = doc.id
+    //         return data
+    //     })
+    //     const locationsDocs = locationsSnapshots.docs.map((doc)=>{
+    //         const data = doc.data()
+    //         data.id = doc.id
+    //         return data
+    //     })
+    //     setProducts(productsDocs)
+    //     setClients(clientsDocs)
+    //     setLocations(locationsDocs)
+    // }
 
-    useEffect(()=>{
-        fetchInfo()
-    }, [])
+    // useEffect(()=>{
+    //     fetchInfo()
+    // }, [])
     
-    return {clients, products, locations}
+    return {}
 }
