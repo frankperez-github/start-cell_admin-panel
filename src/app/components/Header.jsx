ba13cd3997f71be3b34cf973048ad6ec9c59fcda
frankperez-github <fp848584@gmail.com>
2023-08-24 13:04:42 -0400

    connecting everything with firebase

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header()
+{
+    return (
+        <div className="Header">
+            <div className="DesktopHeader">
+                <h1>Start Cell <span>Panel de Administración</span></h1>
+            </div>
+            <div className="MobileHeader"></div>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 'use client'
+import Image from 'next/image'
+import styles from './page.module.css'
+import Header from './components/Header'
+import {v4 as uuidv4} from 'uuid'
+import { useEffect, useState } from 'react'
+import useSiteContext from '@/Hooks/useSiteContext'
+import { db, storage } from './firebase.config'
+import { collection, setDoc, doc, deleteDoc, updateDoc, getDoc } from 'firebase/firestore'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 
 export default function Home() {
 
   return (
     <main>
       <div className="">
-
+        
       </div>
     </main>
     )
