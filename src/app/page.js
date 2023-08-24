012fd2bd332d26233a3152f922888050a3a2ca01
frankperez-github <fp848584@gmail.com>
2023-08-24 13:02:35 -0400

    connecting everything with firebase

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
 'use client'
-import Image from 'next/image'
-import styles from './page.module.css'
-import Header from './components/Header'
-import {v4 as uuidv4} from 'uuid'
-import { useEffect, useState } from 'react'
 
 export default function Home() {
 
   return (
     <main>
       <div className="">
-        
+
       </div>
     </main>
     )
