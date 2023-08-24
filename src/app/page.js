9ac52deac54a73785674eb400ba77da36fee2a90
frankperez-github <fp848584@gmail.com>
2023-08-24 13:31:05 -0400

    connecting everything with firebase

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from './components/Header'
+import {v4 as uuidv4} from 'uuid'
+import { useEffect, useState } from 'react'
+import { db, storage } from './firebase.config'
+
 export default function Home() {
 
   // const {locations, clients, products} = {}
