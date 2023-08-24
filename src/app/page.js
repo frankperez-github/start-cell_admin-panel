072dc2848a690e770d6e5ff21c97d40b01bae136
frankperez-github <fp848584@gmail.com>
2023-08-24 13:06:08 -0400

    connecting everything with firebase

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import styles from './page.module.css'
 import Header from './components/Header'
 import {v4 as uuidv4} from 'uuid'
 import { useEffect, useState } from 'react'
-import useSiteContext from '@/Hooks/useSiteContext'
 import { db, storage } from './firebase.config'
 import { collection, setDoc, doc, deleteDoc, updateDoc, getDoc } from 'firebase/firestore'
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
@@ -14,7 +13,7 @@ export default function Home() {
   return (
     <main>
       <div className="">
-        
+
       </div>
     </main>
     )
